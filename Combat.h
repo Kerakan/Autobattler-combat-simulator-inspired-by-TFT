69c01f75395e5b3f00bf85c2922405954a05e33d
Ignacio Bueno Egea <nacho.bueno.egea@gmail.com>
2026-07-03 12:25:58 +0200

    Refactor combat timing to use chrono library

--- Combat.h
+++ Combat.h
@@ -1,11 +1,11 @@
 #pragma once
-#include <ctime>
 #include <iostream>
 #include "TraitStatSystem.h"
 #include "Team.h"
+#include <chrono>
 #include "AttacksAbilitiesDeathHandling.h"
 void run_combat(std::vector<ChampState> Team1, std::vector<ChampState> Team2) {
-    int combat_start_time = time(0);
+    auto combat_start = std::chrono::steady_clock::now();
     //sum how many of each trait there is
     for (ChampState& champion: Team1){
         for(Trait trait: champion.def.ChampTraits){
@@ -38,48 +38,61 @@ void run_combat(std::vector<ChampState> Team1, std::vector<ChampState> Team2) {
     }
     std::cout<<"Team 2 traits activated"<<std::endl;
     while (true) {
-        int seconds_in_combat = int(time(0) - combat_start_time);
-        std::cout<<"Starting Team1 attacks"<<std::endl;
+        auto now = std::chrono::steady_clock::now();
+        float seconds_in_combat = std::chrono::duration<float>(now - combat_start).count();
         for (ChampState& champion: Team1){
             if (champion.enemytarget ==nullptr){
                 FindClosestEnemy(champion, Team2);
             }
-            autoattack(champion);
-            if (champion.enemytarget->hp_current <= 0){
-                std::cout<<"Champion "<<champion.enemytarget->def.name<<" is dead"<<std::endl;
-                for (ChampState& champ: Team2){
-                    if (champ.enemytarget == champion.enemytarget){
-                        champ.enemytarget = nullptr;
+            if (seconds_in_combat-champion.lastautoattacktime >= 1.0f/champion.attackspeed_current){
+                champion.lastautoattacktime = seconds_in_combat;
+                autoattack(champion);
+                if (champion.enemytarget->hp_current <= 0){
+                    std::cout<<"Champion "<<champion.enemytarget->def.name<<" is dead"<<std::endl;
+                    for (ChampState& champ: Team2){
+                        if (champ.enemytarget == champion.enemytarget){
+                            champ.enemytarget = nullptr;
+                        }
                     }
+                    RemoveDead(Team2);
                 }
-                RemoveDead(Team2);
             }
+            if (champion.mana_current >= champion.def.mana_max){
+                    Ability(champion, Team1, Team2);
+                    champion.mana_current = 0;
+                }
         }
         if (Team2.empty()) {
             std::cout << "Team 1 wins!\n";
             break;
         }
-        std::cout<<"Starting Team2 attacks"<<std::endl;
         for (ChampState& champion: Team2){
             if (champion.enemytarget == nullptr){
                 FindClosestEnemy(champion, Team1);
             }
-            autoattack(champion);
-            if (champion.enemytarget->hp_current <= 0){
-                std::cout<<"Champion "<<champion.enemytarget->def.name<<" is dead"<<std::endl;
-                for (ChampState& champ: Team1){
-                    if (champ.enemytarget == champion.enemytarget){
-                        champ.enemytarget = nullptr;
+            if (seconds_in_combat-champion.lastautoattacktime >= 1.0f/champion.attackspeed_current){
+                champion.lastautoattacktime = seconds_in_combat;
+                autoattack(champion);
+                if (champion.enemytarget->hp_current <= 0){
+                    std::cout<<"Champion "<<champion.enemytarget->def.name<<" is dead"<<std::endl;
+                    for (ChampState& champ: Team1){
+                        if (champ.enemytarget == champion.enemytarget){
+                            champ.enemytarget = nullptr;
+                        }
                     }
+                        RemoveDead(Team1);
                 }
-                RemoveDead(Team1);
             }
+            if (champion.mana_current >= champion.def.mana_max){
+                    Ability(champion, Team2, Team1);
+                    champion.mana_current = 0;
+                }       
         }
         if (Team1.empty()) {
             std::cout << "Team 2 wins!\n";
             break;
         }
-        if (seconds_in_combat > 15) {
+        if (seconds_in_combat > 30) {
             std::cout << "DRAW\n";
             break;
         }
