54ef0c95c4919b400fc8bf4e1950a4429adffbe8
Ignacio Bueno Egea <nacho.bueno.egea@gmail.com>
2026-07-18 10:17:13 +0200

    Delete Log.h

--- Log.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <vector>
-#include <string>
-std::vector <std::string> LogTxts;
-void Log(std::string text){
-    LogTxts.push_back(text);
-    std::cout<<text<<std::endl;
-}
