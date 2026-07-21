b9fe542198818d9302f3a430cb0bbcabc4755089
Ignacio Bueno Egea <nacho.bueno.egea@gmail.com>
2026-07-21 14:58:51 +0200

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -3,8 +3,13 @@
 #include "Champ.h"
 #include "Team.h"
 #include "Combat.h"
+#include "Traits.h"
 #include "Draw.h"
 int main(){
+	LoadTraits();
+	std::cout << "Traits loaded: " << TRAIT_STORAGE.size() << std::endl;
+	LoadChampions();
+	std::cout << "Champions loaded: " << CHAMP_POOL.size() << std::endl;
 	std::vector<ChampState> TeamA = CreateTeam();
 	std::vector<ChampState> TeamB = CreateTeam();	
 	run_combat(TeamA, TeamB);
